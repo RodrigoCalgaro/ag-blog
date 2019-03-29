7131c39d2b4edf7e5e406d1909f385cf9b9f0525
Rodrigo Calgaro <ri.calgaro@gmail.com>
2019-03-28 21:07:03 -0300

    Post Unidad 2

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,6 @@ var express = require('express');
 var router = express.Router();
 var models = require('./../models')
 var Comentario = models.Comentario
-/* 
-var comments = [{
-  id: 1,
-  postId: 1,
-  nombre: 'Juan Alberto',
-  fecha: new Date("2019-3-13"),
-  comentario: 'Excelente artículo espero mas material.'
-}, {
-  id: 2,
-  postId: 1,
-  nombre: 'Fernando Gago',
-  fecha: new Date("2019-3-13"),
-  comentario: 'Cambio.'
-}, {
-  id: 3,
-  postId: 1,
-  nombre: 'Marcelo Benedetto',
-  fecha: new Date("2019-3-14"),
-  comentario: 'Y va el tercero y va el tercero..'
-}] */
 
 
 /* GET home page. */
@@ -50,6 +30,19 @@ router.get('/2', function (req, res, next) {
     }
   }).then(comments => {
     res.render('2-Unidad1', {
+      comments
+    });
+  })
+});
+
+/* GET Post: Unidad 2. */
+router.get('/3', function (req, res, next) {
+  Comentario.findAll({
+    where: {
+      postId: 3
+    }
+  }).then(comments => {
+    res.render('3-Unidad2', {
       comments
     });
   })
